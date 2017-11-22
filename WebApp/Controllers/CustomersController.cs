439ebd789a7f76aacb13e9e33d00fe0b6b28fd70
santoshgyawali <santosh.g2047@gmail.com>
2017-11-22 17:47:33 -0600

    add project in the new repo

--- WebApp/Controllers/CustomersController.cs
+++ WebApp/Controllers/CustomersController.cs
@@ -4,21 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApp.Models;
+using System.Data.Entity;
 
 namespace WebApp.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context; // we need dbcontext to access the database
+        public CustomersController()//initialize dbcontext in constructor
+        {
+            _context = new ApplicationDbContext();
+
+        }
+        //dbcontext is the disposable object so we need to properly dispose it
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         public ViewResult Index()
         {
-            var customers = GetCustomers();
-
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();//when this line is executed entity framwork in not going to query to DB (differed execution)
+                                                        //but after Tolist() query is executed from this line
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
@@ -26,13 +38,13 @@ namespace WebApp.Controllers
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
+      /*  private IEnumerable<Customer> GetCustomers()  //hardcoded method for accessing data
         {
             return new List<Customer>
             {
                 new Customer { Id = 1, Name = "John Smith" },
                 new Customer { Id = 2, Name = "Mary Williams" }
             };
-        }
+        }*/
     }
 }
--- WebApp/Models/Customer.cs
+++ WebApp/Models/Customer.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApp.Models
 {
     public class Customer
     {
         public int Id  { get; set; }
+        [Required]
+        [StringLength(255)]
         public String Name { get; set; }
+        public bool IsSubsribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
+        public DateTime Dob { get; set; }
     }
 }
